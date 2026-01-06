d96bc8a794f7adaf27e629121dc29ba832625515
Sanjana Kulasooriya <nethmikulasooriya51@gmail.com>
2026-01-06 22:54:07 +0530

    Add files via upload

---
+++ task 1 sql query.sql
@@ -0,0 +1,241 @@
+select*
+from [Health_Data].[dbo].[health 1]
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Total Population] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Total Population] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Male Population] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Male Population] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Female Population] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Female Population] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Aboriginal population proportion (%)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Aboriginal population proportion (%)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 0-4 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 0-4 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 5-9 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 5-9 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 15-19 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 15-19 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 20-24 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 20-24 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 25-29 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 25-29 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 30-34 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 30-34 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 35-39 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 35-39 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 40-44 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 40-44 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 45-49 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 45-49 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 50-54 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 50-54 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 55-59 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 55-59 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 60-64 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 60-64 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 65-69 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 65-69 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 70-74 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 70-74 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 75-79 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 75-79 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 80-84 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 80-84 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Population aged 85+ years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Population aged 85+ years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Age specific birth rate per 1000 women aged 15-44 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Age specific birth rate per 1000 women aged 15-44 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Age specific birth rate per 1000 women aged 15-19 years] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Age specific birth rate per 1000 women aged 15-19 years] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Low birth weight (%)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Low birth weight (%)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Still birth (%)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Still birth (%)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Fertility rate] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Fertility rate] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Total potentially preventable hospitalisations (PPH) (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Total potentially preventable hospitalisations (PPH) (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [PPH - Vaccine preventable conditions (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [PPH - Vaccine preventable conditions (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [PPH - Acute conditions (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [PPH - Acute conditions (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [PPH - Chronic conditions (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [PPH - Chronic conditions (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Total injury hospitalisations (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Total injury hospitalisations (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Transport accident hospitalisations (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Transport accident hospitalisations (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Accidental fall hospitalisations (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Accidental fall hospitalisations (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Exposure to mechanical force hospitalisations (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Exposure to mechanical force hospitalisations (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Accidental poisoning hospitalisations (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Accidental poisoning hospitalisations (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Intentional self-harm hospitalisations (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Intentional self-harm hospitalisations (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Assault hospitalisations (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Assault hospitalisations (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Adverse effects due to drugs and other substance hospitalisations (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Adverse effects due to drugs and other substance hospitalisations (ASR per 100,000)] IS NULL;
+
+UPDATE [Health_Data].[dbo].[health 1]
+SET 
+    [Abnormal reaction following procedure hospitalisations (ASR per 100,000)] = '0'
+FROM [Health_Data].[dbo].[health 1]
+WHERE [Abnormal reaction following procedure hospitalisations (ASR per 100,000)] IS NULL;
+
+	select*
+	from [Health_Data].[dbo].[health 1]
+
+
